0366d03dd7b49bc117b6aaaed5de32532e91ccaa
Petr Korolev <sky4winder@gmail.com>
2015-06-17 15:34:21 +0300

    add SWGaugeKit framework

---
+++ SWGaugeKit/SWGaugeKit/SWGaugeKit.h
@@ -0,0 +1,19 @@
+//
+//  SWGaugeKit.h
+//  SWGaugeKit
+//
+//  Created by Petr Korolev on 17/06/15.
+//  Copyright (c) 2015 Petr Korolev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWGaugeKit.
+FOUNDATION_EXPORT double SWGaugeKitVersionNumber;
+
+//! Project version string for SWGaugeKit.
+FOUNDATION_EXPORT const unsigned char SWGaugeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWGaugeKit/PublicHeader.h>
+
+
